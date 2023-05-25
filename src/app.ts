6fe81d1bb2865c7b90c6a08b70ecac894f642257
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2023-05-24 23:50:33 -0300

    Implementando JWT do fastify

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import fastify from "fastify";
 import { ZodError } from "zod";
+import fastifyJwt from "@fastify/jwt";
 
-import { appRoutes } from "@/http/routes";
 import { env } from "./env";
+import { appRoutes } from "@/http/routes";
 
 export const app = fastify();
+
+app.register(fastifyJwt, {
+    secret: env.JWT_SECRET,
+});
 
 app.register(appRoutes, { prefix: '/api/v1/' });
 
--- src/http/routes/routes.ts
+++ src/http/routes/routes.ts
@@ -1,12 +1,16 @@
 import { FastifyInstance } from "fastify";
 
 import {
+    profileController,
     registerController,
     authenticateController,
 } from "@/http/controller";
 
 export const appRoutes = async (app: FastifyInstance) => {
+    /** Não precisa de autenticação */
     app.post('/users/register', registerController.register);
-
     app.post('/sessions', authenticateController.authenticate);
+
+    /** Só quando estiver autenticado */
+    app.get('/profile/me', profileController.profile);
 };
